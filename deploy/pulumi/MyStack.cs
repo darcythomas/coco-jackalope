db0e8dbbf8c2f1af1adf6aab779939908d447496
Darcy <github@darcythomas.com>
2021-05-22 21:50:13 +1200

    deploys basic 'site'  to s3 bucket

--- deploy/pulumi/MyStack.cs
+++ deploy/pulumi/MyStack.cs
@@ -1,17 +1,46 @@
 using Pulumi;
 using Pulumi.Aws.S3;
+using Pulumi.Aws.S3.Inputs;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        // Create an AWS resource (S3 Bucket)
-        var bucket = new Bucket("some-bucket");
+        var bucket = new Bucket("some-bucket", new BucketArgs
+        {
+            Website = new BucketWebsiteArgs
+            {
+                IndexDocument = "index.html"
+            }
+        });
+
+
+        var bucketObjectIndex = new BucketObject("index.html", new BucketObjectArgs
+        {
+            Acl = "public-read",
+            ContentType = "text/html",
+            Bucket = bucket.BucketName,
+            Source = new FileAsset("index.html")
+        });
+
+        var bucketObjectFavicon = new BucketObject("favicon.ico", new BucketObjectArgs
+        {
+            Acl = "public-read",
+            ContentType = "image/ico",
+            Bucket = bucket.BucketName,
+            Source = new FileAsset("favicon.ico")
+        });
+
 
         // Export the name of the bucket
         this.BucketName = bucket.Id;
+        this.BucketEndpoint = Output.Format($"http://{bucket.WebsiteEndpoint}");
+
     }
 
     [Output]
     public Output<string> BucketName { get; set; }
+
+    [Output]
+    public Output<string> BucketEndpoint { get; set; }
 }
