af159f2a564e3e36b3b89d37f9bedd74a3705a1b
Darcy <github@darcythomas.com>
2021-05-23 22:38:50 +1200

    tidy

--- deploy/pulumi/MyStack.cs
+++ deploy/pulumi/MyStack.cs
@@ -23,30 +23,11 @@ class MyStack : Stack
 
             }
         });
-
-        //SetBucketPolicy(bucket);
-
-        // var bucketObjectIndex = new BucketObject("index.html", new BucketObjectArgs
-        // {
-        //     Acl = "public-read",
-        //     ContentType = "text/html",
-        //     Bucket = bucket.BucketName,
-        //     Source = new FileAsset("index.html")
-        // });
-
-        // var bucketObjectFavicon = new BucketObject("favicon.ico", new BucketObjectArgs
-        // {
-        //     Acl = "public-read",
-        //     ContentType = "image/ico",
-        //     Bucket = bucket.BucketName,
-        //     Source = new FileAsset("favicon.ico")
-        // });
-
-        Regex rx = new Regex(@"^(.*)(wwwroot)(?<filePath>.*)",
-                  RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
+        
+        Regex rx = new Regex(@"^(.*)(wwwroot)(?<filePath>.*)",  RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         var files = Directory.GetFiles("../frontend/wwwroot", "*", SearchOption.AllDirectories);
+        
         foreach (var file in files)
         {
             var name = Path.GetFileName(file);
@@ -76,48 +57,11 @@ class MyStack : Stack
         this.BucketName = bucket.Id;
         this.BucketEndpoint = Output.Format($"http://{bucket.WebsiteEndpoint}");
 
-    }
+    
 
 
 
-    private void SetBucketPolicy(Bucket bucket)
-    {
-
-        var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
-        {
-            Bucket = bucket.Id,
-            Policy = Output.Tuple(bucket.Arn, bucket.Arn).Apply(values =>
-            {
-                var bucketArn = values.Item1;
-                var bucketArn1 = values.Item2;
-                return JsonSerializer.Serialize(new Dictionary<string, object?>
-                {
-                    { "Version", "2012-10-17" },
-                    { "Id", "CocojackalopeAllowGetOnSite" },
-                    { "Statement", new[]
-                        {
-                            new Dictionary<string, object?>
-                            {
-                                { "Effect", "Allow" },
-                                { "Principal", "*" },
-                                { "Action", "s3:GetObject" },
-                                { "Resource", new[]
-                                    {
-                                        bucketArn,
-                                        $"{bucketArn1}/*",
-                                    }
-                                 },
-
-                            },
-                        }
-                     },
-                });
-            }),
-        });
-
-
-
-
+    
 
     }
 
