72b52000a969042d50cbce6312252e85f3e66d12
Darcy <github@darcythomas.com>
2021-08-20 20:52:18 +1200

    update to .net6 preview 7

---
+++ src/cms/Client/PublicClient.cs
@@ -0,0 +1,12 @@
+
+using System.Net.Http;
+
+public class PublicClient
+{
+    public HttpClient Client { get; }
+    
+    public PublicClient(HttpClient httpClient)
+    {
+        Client = httpClient;
+    }
+}
